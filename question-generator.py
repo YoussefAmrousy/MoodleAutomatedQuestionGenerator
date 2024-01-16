2b52b3b844df5f455b6b733ac8ca7f5b12e9e557
YoussefAmrousy <youssef.amrousy@gmail.com>
2024-01-16 20:25:13 +0200

    Adjusted question generator script to work with php

--- question-generator.py
+++ question-generator.py
@@ -1,10 +1,11 @@
 from transformers import pipeline
 import torch
 import fitz
+import sys
 
-def file_to_text(file):
+def file_to_text(filepath):
     text = ""
-    with fitz.open(file) as pdf_document:
+    with fitz.open(filepath) as pdf_document:
         num_pages = pdf_document.page_count
 
         for page_num in range(num_pages):
@@ -13,8 +14,8 @@ def file_to_text(file):
 
     return text
 
-def generate_questions(file):
-    input_text = file_to_text(file)
+def generate_questions(filepath):
+    input_text = file_to_text(filepath)
 
     seed_value = hash(input_text) % (2**32 - 1)
     torch.manual_seed(seed_value)
@@ -31,4 +32,12 @@ def generate_questions(file):
         answer = qa_result['answer']
         generated_question_answers.append((generated_question, answer))
 
-    return generated_question_answers
+    print(generated_question_answers)
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python generate_questions_script.py <file_path>")
+        sys.exit(1)
+
+    file_path = sys.argv[1]
+    generate_questions(file_path)
