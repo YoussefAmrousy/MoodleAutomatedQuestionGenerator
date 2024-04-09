1b412f3f0efd22c3b52acd238fd3bfbe7034d163
YoussefAmrousy <youssef.amrousy@gmail.com>
2024-04-09 05:12:56 +0200

    updated short answer & true-false files

--- local/questiongenerator/scripts/short-answer.py
+++ local/questiongenerator/scripts/short-answer.py
@@ -1,64 +1,60 @@
 import sys
-from transformers import pipeline
 import json
-from shared import *
+from transformers import pipeline
+from shared import file_to_text, check_difficulty, save_questions_to_xml
 
 generated_question_answers = []
 
 
-def create_questions_shortanswer(input_text: str, questionsNum: int,
-                                 question_generator_pipeline: any):
+def create_questions_shortanswer(input_text: str, questions_num: int,
+                                 question_generator: pipeline):
     if len(input_text) <= 512:
-        questions = question_generator_pipeline(
-            input_text,
-            max_length=30,
-            num_return_sequences=questionsNum,
-            num_beams=10)
-        created_questions = [
-            question['generated_text'].strip().capitalize()
-            for question in questions
-        ]
+        questions = question_generator(input_text,
+                                       max_length=30,
+                                       num_return_sequences=questions_num,
+                                       num_beams=10)
     else:
         text_segments = [
             input_text[i:i + 512] for i in range(0, len(input_text), 512)
         ]
         created_questions = []
         for segment in text_segments:
-            questions = question_generator_pipeline(segment,
-                                                    max_length=30,
-                                                    num_return_sequences=1,
-                                                    num_beams=10)
-            for question in questions:
-                created_questions.append(
-                    question['generated_text'].strip().capitalize())
+            questions = question_generator(segment,
+                                           max_length=30,
+                                           num_return_sequences=1,
+                                           num_beams=10)
 
-            if len(created_questions) >= questionsNum:
+            if len(created_questions) >= questions_num:
                 break
+
+        created_questions = [
+            question['generated_text'].strip().capitalize()
+            for question in questions
+        ]
 
     return created_questions
 
 
 def create_answer(input_text: str, question: str,
-                  question_answer_pipeline: any):
+                  question_answer_pipeline: pipeline):
     qa_result = question_answer_pipeline(question=question, context=input_text)
     return qa_result['answer'].replace('\n', ' ').capitalize()
 
 
-def generate_questions(filepath: str, questionsNum: int, difficulty: str):
+def generate_questions(filepath: str, questions_num: int, difficulty: str):
     input_text: str = file_to_text(filepath)
 
-    question_generator_pipeline = pipeline(
-        "text2text-generation", model="voidful/bart-eqg-question-generator")
+    question_generator = pipeline("text2text-generation",
+                                  model="voidful/bart-eqg-question-generator")
     question_answer_pipeline = pipeline(
         "question-answering",
         model="bert-large-uncased-whole-word-masking-finetuned-squad")
 
-    questions = create_questions_shortanswer(input_text, questionsNum,
-                                             question_generator_pipeline)
+    questions = create_questions_shortanswer(input_text, questions_num,
+                                             question_generator)
     for question in questions:
         answer = create_answer(input_text, question, question_answer_pipeline)
-        difficultyCheck = check_difficulty(question, answer, difficulty)
-        if (difficultyCheck):
+        if check_difficulty(question, answer, difficulty):
             generated_question_answers.append((question, answer, difficulty))
 
     questions_json = json.dumps(generated_question_answers)
@@ -70,17 +66,15 @@ def generate_questions(filepath: str, questionsNum: int, difficulty: str):
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print(
-            "Usage: python generate_questions_script_path.py <file_path> <questionsNum> <difficulty>"
+            "Usage: python generate_questions_script_path.py <file_path> <questions_num> <difficulty>"
         )
         sys.exit(1)
 
     file_path = sys.argv[1]
-    questionsNum = int(sys.argv[2])
+    questions_num = int(sys.argv[2])
     difficulty = sys.argv[3]
 
-    generated_question_answers = generate_questions(file_path, questionsNum,
+    generated_question_answers = generate_questions(file_path, questions_num,
                                                     difficulty)
 
-    save_questions_to_xml(
-        generated_question_answers,
-        "generated_questions")  # To save in the moodle question bank
+    save_questions_to_xml(generated_question_answers, "generated_questions")
--- local/questiongenerator/scripts/true-false.py
+++ local/questiongenerator/scripts/true-false.py
@@ -1,65 +1,56 @@
 from transformers import pipeline
-import torch
 import sys
-import fitz
 import json
+from shared import file_to_text
 
 
-def extract_text_from_page(page):
-    return page.get_text()
+def create_questions_true_false(input_text: str, questions_num: int,
+                                question_generator_pipeline: pipeline):
+    text_segments = [
+        input_text[i:i + 512] for i in range(0, len(input_text), 512)
+    ]
+    num_beams = max(1, questions_num // len(text_segments))
+    generated_questions = []
+    for segment in text_segments:
+        questions = question_generator_pipeline(segment,
+                                                max_length=100,
+                                                num_return_sequences=1,
+                                                num_beams=num_beams)
+
+        generated_questions.extend([
+            question['generated_text'].strip().capitalize()
+            for question in questions
+        ])
+
+        if len(generated_questions) >= questions_num:
+            return generated_questions[:questions_num]
+
+    return generated_questions
 
 
-def file_to_text(filepath):
-    text = ""
-    with fitz.open(filepath) as pdf_document:
-        num_pages = pdf_document.page_count
-
-        for page_num in range(num_pages):
-            page = pdf_document[page_num]
-            text += extract_text_from_page(page)
-
-    return text
+def create_answer(input_text: str, question: str,
+                  question_answer_pipeline: any):
+    qa_result = question_answer_pipeline(question=question, context=input_text)
+    return qa_result['answer'].replace('\n', ' ').capitalize()
 
 
-def generate_true_false_questions(filepath, questionsNum):
+def generate_true_false_questions(filepath, questions_num):
     input_text = file_to_text(filepath)
-
-    seed_value = hash(input_text) % (2**32 - 1)
-    torch.manual_seed(seed_value)
 
     question_generator = pipeline(
         "text2text-generation",
         model="mrm8488/t5-base-finetuned-question-generation-ap")
+
     qa_generator = pipeline("question-answering",
                             model="deepset/roberta-base-squad2")
 
+    questions = create_questions_true_false(input_text, questions_num,
+                                            question_generator)
     generated_question_answers = []
-
-    text_segments = [
-        input_text[i:i + 512] for i in range(0, len(input_text), 512)
-    ]
-
-    for segment in text_segments:
-        num_beams = max(1, questionsNum // len(text_segments))
-
-        questions = question_generator(segment,
-                                       max_length=100,
-                                       num_return_sequences=1,
-                                       num_beams=num_beams)
-        for question in questions:
-            question_text = question['generated_text'].strip().capitalize()
-            qa_result = qa_generator(question=question_text, context=segment)
-            answer = qa_result['answer'].strip().capitalize()
-            is_true = answer.lower() in question_text.lower()
-            generated_question_answers.append([question_text, is_true])
-
-        if len(generated_question_answers) >= questionsNum:
-            break
-
-    generated_question_answers = generated_question_answers[:questionsNum]
-
-    output_json = json.dumps(generated_question_answers)
-    print(output_json)
+    for question in questions:
+        answer = create_answer(input_text, question, qa_generator)
+        is_true = answer.lower() in question.lower()
+        generated_question_answers.append([question, is_true])
 
     return generated_question_answers
 
@@ -67,12 +58,15 @@ def generate_true_false_questions(filepath, questionsNum):
 if __name__ == "__main__":
     if len(sys.argv) != 3:
         print(
-            "Usage: python generate_questions_script_path.py <file_path> <questionsNum>"
+            "Usage: python generate_questions_script_path.py <file_path> <questions_num>"
         )
         sys.exit(1)
 
     file_path = sys.argv[1]
-    questionsNum = int(sys.argv[2])
+    questions_num = int(sys.argv[2])
 
     generated_question_answers = generate_true_false_questions(
-        file_path, questionsNum)
+        file_path, questions_num)
+
+    output_json = json.dumps(generated_question_answers)
+    print(output_json)
